d29df5a809f0f561cf2b2f745f771903cbe595f2
Tomas Miguel <TomasDuarteMiguel2006@gmail.com>
2024-12-04 21:34:00 +0000

    tomas

--- ex1.sh
+++ ex1.sh
@@ -2,9 +2,9 @@
 
 for pasta1 in ./script_sample_folder/*; do
     # Repetir o processo para cada letra do alfabeto
-    
-         touch $pasta1/teste.txt
+        nome=$(basename "$dir")
+         touch $pasta1/script_sample_folder-$nome.txt
     done
 
 # Mensagem de sucesso
-echo "Pastas criadas com sucesso."
+echo "Txt criado com sucesso."
