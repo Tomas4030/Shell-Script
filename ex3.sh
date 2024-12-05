db93102bb055889eca11eac0b811e6602a72dff3
Tomas4030 <101940482+Tomas4030@users.noreply.github.com>
2024-12-05 23:35:55 +0000

    Update ex3.sh

--- ex3.sh
+++ ex3.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+if [ $# -eq 0 ]; then
+  echo "Uso: $0 <caractere>"
+  exit 1
+fi
+
+caractere=$1
+diretorio_base="script_sample_folder"
+
+if [ ! -d "$diretorio_base" ]; then
+  echo "Erro: O diretório $diretorio_base não existe."
+  exit 1
+fi
+
+for subdiretorio in "$diretorio_base"/*; do
+  if [ -d "$subdiretorio" ] && [[ "$subdiretorio" == *"$caractere"* ]]; then
+    echo "Deletando o conteúdo de: $subdiretorio"
+    rm -rf "$subdiretorio"/*
+  fi
+done
