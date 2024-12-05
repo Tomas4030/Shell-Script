e1bb8398224dbf8210084eed2df53935540c0103
Tomas4030 <101940482+Tomas4030@users.noreply.github.com>
2024-12-05 23:35:37 +0000

    Update ex2.sh

--- ex2.sh
+++ ex2.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
-for pasta1 in ./script_sample_folder/*; do
-    # Repetir o processo para cada letra do alfabeto
-        nome=$(basename "$dir")
-        rename $pasta1/script_sample_folder-$nome.txt
-    done
+#So funciona uma vez Ex
+# primeira vez que renomeia Prefixo-aa
+# segunda vez que renomeia Prefixo-Prefixo_anterior-aa
+# e por ai gg desito 
 
-# Mensagem de sucesso
-echo "Txt criado com sucesso."
+if [ -z "$1" ]; then
+    echo "Uso: $0 <prefixo>"
+    exit 1
+fi
+
+PREFIXO=$1
+PASTA_BASE="script_sample_folder"
+
+for DIR in "$PASTA_BASE"/*/; do
+    if [ -d "$DIR" ]; then
+        
+        NOME_ATUAL=$(basename "$DIR")
+        NOVO_NOME="$PASTA_BASE/$PREFIXO-$NOME_ATUAL"
+        mv "$DIR" "$NOVO_NOME"
+    fi
+done
+
+echo "Pastas renomeadas com sucesso."
