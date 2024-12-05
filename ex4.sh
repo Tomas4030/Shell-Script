b6040f297000be4293e384621b11b08a94235e03
Tomas4030 <101940482+Tomas4030@users.noreply.github.com>
2024-12-05 23:36:09 +0000

    Update ex4.sh

--- ex4.sh
+++ ex4.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+
+# Verifica se o argumento foi passado
+if [ $# -eq 0 ]; then
+  echo "Uso: $0 <V|F>"
+  exit 1
+fi
+
+diretorio_base="script_sample_folder"
+
+# Deleta os diretórios com base no argumento
+if [ "$1" == "V" ]; then
+  rm -rf "$diretorio_base"/*
+  echo "Todos os diretórios foram deletados."
+else
+  for subdiretorio in "$diretorio_base"/*; do
+    [ -d "$subdiretorio" ] && [ "$(ls -A "$subdiretorio")" ] && rm -rf "$subdiretorio"
+  done
+  echo "Diretórios com conteúdo foram deletados."
+fi
